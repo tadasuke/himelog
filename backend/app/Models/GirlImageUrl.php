6fb5c505754db54f05625ecd14c5edd52eb7687a
tadasuke <tadasuketadasuke@gmail.com>
2025-11-29 12:38:31 +0900

    もろもろ対応

--- backend/app/Models/GirlImageUrl.php
+++ backend/app/Models/GirlImageUrl.php
@@ -54,3 +54,4 @@ class GirlImageUrl extends Model
 
 
 
+
--- backend/config/cors.php
+++ backend/config/cors.php
@@ -4,7 +4,10 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173', 'http://localhost:8000'],
-    'allowed_origins_patterns' => ['#^https?://.*\.madfaction\.net$#'],
+    'allowed_origins_patterns' => [
+        '#^https?://.*\.madfaction\.net$#',  // 開発環境用
+        '#^https?://.*\.hime-log\.jp$#',     // 本番環境用（追加）
+    ],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
